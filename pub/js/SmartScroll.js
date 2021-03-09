5fa8234d8e37a354eaf619d85071ebd03006e022
Joshua Chua <winwin.chua@gmail.com>
2021-03-09 14:22:48 -0500

    added example html, and added updatePreview function

--- pub/js/SmartScroll.js
+++ pub/js/SmartScroll.js
@@ -2,7 +2,7 @@
 "use strict";
 
 
-//import html2canvas from './html2canvas.esm.js'; 
+import html2canvas from './html2canvas.esm.js'; 
 
 const SmartScroll = function() {
 
@@ -21,6 +21,7 @@ const SmartScroll = function() {
 
         const modalContent = document.createElement("div"); 
         modalContent.className = "modal-content"; 
+        modalContent.id = "SmartScroll-content"; 
 
         const settingsIcon = document.createElement("button"); 
         settingsIcon.innerHTML = "&#9881;"; 
@@ -32,23 +33,51 @@ const SmartScroll = function() {
         closeButton.className = "button close"; 
         closeButton.onclick = _self.close;
 
-
         modalContent.appendChild(settingsIcon); 
         modalContent.appendChild(closeButton); 
+
         modalDiv.appendChild(modalContent); 
-
-        //TO-DO: initialize preview canvas 
-        // html2canvas(document.body).then(function(canvas) {
-        //     document.body.appendChild(canvas);
-        // });
-
 
         //TO-DO: initialize time tracker canvas
 
         //add modal to DOM (in html body) 
         const body = document.getElementsByTagName("BODY")[0];
         body.appendChild(modalDiv); 
+
+        //TO-DO: initialize preview canvas 
+        _self.updatePreviewCanvas(); 
 	}
+
+    _self.updatePreviewCanvas = function() {
+        
+        var previewArea = document.body.getBoundingClientRect(); 
+
+        console.log("original body width: " + previewArea.width); 
+        console.log("original body height: " + previewArea.height);
+
+        var width =  Math.min(3000, previewArea.width); 
+        var height = Math.min(20000, previewArea.height); 
+
+        var contentDiv = document.getElementById("SmartScroll-content");
+        
+        //remove existing canvas if exists 
+        var existingCanvas = document.getElementById("SmartScroll-canvas"); 
+        if(existingCanvas !== null) {
+            existingCanvas.remove(); 
+        }
+
+        html2canvas(document.body, {height: height, width: width}).then(function(canvas) {
+
+            var newHeight = (0.9 * window.innerHeight); 
+            var newWidth = (0.1 * window.innerWidth); 
+
+            canvas.style="width: " + newWidth + "px; height: " + newHeight + "px;"
+        
+            contentDiv.appendChild(canvas); 
+        });
+
+
+    }
 
     //open smartscroll 
     _self.open = function() { 
