0b0aac8d93a4c0380d74edab6d171d1b5ec6f9cb
Joshua Chua <winwin.chua@gmail.com>
2021-05-09 20:26:37 -0400

    misc server change

--- server/server.js
+++ server/server.js
@@ -11,6 +11,7 @@ const buildPath = path.join(__dirname,  "/build");
 app.use(express.static(buildPath));
 app.use("/static", express.static(path.join(__dirname, "build/static")));
 
+app.get('*', (req,res) => res.sendFile(path.join(__dirname+'/build/index.html'))); 
 
 const port = process.env.PORT || 5000
 app.listen(port, () => {
